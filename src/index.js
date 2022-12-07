06300e1b05fe3ff0d9bf054e33ca4c8a793cbc20
Mipmipp <106466126+Mipmipp@users.noreply.github.com>
2022-12-07 08:47:17 -0300

    Upload index.js with bootstrap loaded and react-query for cache storage (also default options of it)

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient({
+   defaultOptions: {
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      refetchOnWindowFocus: false,
+      staleTime: Infinity,
+      cacheTime: 18000,
+   },
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+   <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+         <App />
+      </QueryClientProvider>
+   </React.StrictMode>
+);
+
+reportWebVitals();
