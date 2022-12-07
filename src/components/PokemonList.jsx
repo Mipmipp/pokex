7f725b18506af07e412660e18aac02c1ed0ac89b
Mipmipp <106466126+Mipmipp@users.noreply.github.com>
2022-12-07 08:49:42 -0300

    Create PokemonList component

---
+++ src/components/PokemonList.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useQuery } from "react-query";
+import Row from "react-bootstrap/Row";
+import PokemonOfList from "./PokemonOfList";
+import PokemonCard from "./PokemonCard";
+import ButtonLeft from "./small-components/ButtonLeft";
+import ButtonRight from "./small-components/ButtonRight";
+import Spinner from "react-bootstrap/Spinner";
+import Alert from "react-bootstrap/Alert";
+import "./PokemonList.css";
+
+const PokemonList = () => {
+};
+
+export default PokemonList;
