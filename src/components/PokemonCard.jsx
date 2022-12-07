2ca1c0e20b52a00527a5ad0a821b40d52c031ac8
Mipmipp <106466126+Mipmipp@users.noreply.github.com>
2022-12-07 09:05:46 -0300

    Create PokemonCard component
    It will be use for show a big card where will be the information of every pokemon clicked by the user

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useQuery } from "react-query";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "./PokemonCard.css";
+
+const PokemonCard = ({ urlPokemonClicked }) => {
+   }
+};
+
+export default PokemonCard;
