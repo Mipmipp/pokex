c5968bd43e1cd976d41954b3e7fc64bba84eda11
Mipmipp <106466126+Mipmipp@users.noreply.github.com>
2022-12-07 09:04:18 -0300

    Load a small card with pokemon's image and name.
    This component have a state where it split the url to get the pokemonID and use it in the src for the image

--- src/components/PokemonOfList.jsx
+++ src/components/PokemonOfList.jsx
@@ -5,6 +5,23 @@ import Card from "react-bootstrap/Card";
 import "./PokemonOfList.css";
 
 const PokemonOfList = ({ name, url, pokemonClicked }) => {
+   const [pokemonID] = useState(`${url.split("/")[6]}`);
+
+   return (
+      <Col className="pokemon-body" xs={6} sm={3}>
+         <Card className="pokemon" border="dark" onClick={pokemonClicked}>
+            <Card.Img
+               className="pokemon-image"
+               variant="top"
+               alt={name}
+               src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonID}.png`}
+            />
+            <Card.Body className="pokemon-of-list-body">
+               <Card.Title className="pokemon-name">{name}</Card.Title>
+            </Card.Body>
+         </Card>
+      </Col>
+   );
 };
 
 export default PokemonOfList;
