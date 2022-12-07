ef0c1cd75b9cf2c0750269a8ae465b5d5b601d7f
Mipmipp <106466126+Mipmipp@users.noreply.github.com>
2022-12-07 08:50:27 -0300

    Declares some useState

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -12,6 +12,17 @@ import Alert from "react-bootstrap/Alert";
 import "./PokemonList.css";
 
 const PokemonList = () => {
+   const [pokemons, setPokemons] = useState([]);
+   const [actualPage, setActualPage] = useState(
+      "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=16"
+   );
+   const [pokemonClicked, setPokemonClicked] = useState(
+      "https://pokeapi.co/api/v2/pokemon/1/"
+   );
+   const [previous, setPrevious] = useState("");
+   const [next, setNext] = useState("");
+   const [variantButtonLeft, setVariantButtonLeft] = useState("dark");
+   const [variantButtonRight, setVariantButtonRight] = useState("dark");
 };
 
 export default PokemonList;
