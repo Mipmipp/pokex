239ddd2d018a33fea4298043b1541ac8a3517673
Mipmipp <106466126+Mipmipp@users.noreply.github.com>
2022-12-07 09:02:27 -0300

    Create pokemonOfList component
    It will be use for every pokemon of the pokemon list

---
+++ src/components/PokemonOfList.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useState } from "react";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import "./PokemonOfList.css";
+
+const PokemonOfList = ({ name, url, pokemonClicked }) => {
+};
+
+export default PokemonOfList;
