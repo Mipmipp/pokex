5d58c5d3372c4451555e8c09ff4e9d0de9a8825c
Mipmipp <106466126+Mipmipp@users.noreply.github.com>
2022-12-07 08:43:03 -0300

    Create Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+   return (
+      <header id="header">
+         <div>
+            <h1>PokeDex</h1>
+         </div>
+      </header>
+   );
+};
+
+export default Header;
