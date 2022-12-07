099203a0f346edbd2c30763cac811a53a13714d2
Mipmipp <106466126+Mipmipp@users.noreply.github.com>
2022-12-07 20:48:56 -0300

    Simplifies import lines

--- src/components/PokemonOfList.jsx
+++ src/components/PokemonOfList.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import Col from "react-bootstrap/Col";
-import Card from "react-bootstrap/Card";
+import { Col, Card } from "react-bootstrap";
 import "./PokemonOfList.css";
 
 const PokemonOfList = ({ name, url, pokemonClicked }) => {
