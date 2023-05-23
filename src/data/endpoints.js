89756715c78c16d4891870ad932acd08dd9c559d
Mipmipp <106466126+Mipmipp@users.noreply.github.com>
2023-05-22 23:57:51 -0300

    Create a file with endpoints

---
+++ src/data/endpoints.js
@@ -0,0 +1,9 @@
+export const FIRST_PAGE_ENDPOINT = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=12";
+
+export const FIRST_POKEMON_ENDPOINT = 'https://pokeapi.co/api/v2/pokemon/bulbasaur/';
+
+export const POKEMON_PICTURE_ENDPOINT = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/";
+
+export const ALL_POKEMONS_ENDPOINT = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
+
+export const POKEMON_FLAVOR_TEXT_ENDPOINT = "https://pokeapi.co/api/v2/pokemon-species/bulbasaur/";
