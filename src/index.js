2c3c6a1cebf9fc06f62dcdebbfbf782d17f9a5ca
Mipmipp <106466126+Mipmipp@users.noreply.github.com>
2023-05-23 00:22:30 -0300

    Update Index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App";
-import "bootstrap/dist/css/bootstrap.min.css";
 import { QueryClient, QueryClientProvider } from "react-query";
+import App from "./components/App";
+import { reactQueryConfiguration } from "./data/reactQueryConfiguration";
+
 // (delete this to use react-querydeveloper tools)
 // import { ReactQueryDevtools } from "react-query/devtools";
 // (then add the next line below <app />)
 // <ReactQueryDevtools />
 
 const queryClient = new QueryClient({
-   defaultOptions: {
-      refetchOnMount: false,
-      refetchOnReconnect: false,
-      refetchOnWindowFocus: false,
-      staleTime: Infinity,
-      cacheTime: 18000,
-   },
+   defaultOptions: reactQueryConfiguration
 });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
