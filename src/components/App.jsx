c74cc2832d6e628bc68ad97ed3bc0c5485258a78
Mipmipp <106466126+Mipmipp@users.noreply.github.com>
2023-05-23 00:06:50 -0300

    Rework App with new components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
 import React from "react";
-import Header from "./Header";
-import PokemonList from "./PokemonList";
-import Footer from "./Footer";
+import { theme } from "../data/theme.js";
+import { Experimental_CssVarsProvider as CssVarsProvider } from "@mui/material/styles";
+import { PokedexProvider } from "../hooks/PokedexProvider.jsx";
+import { Main } from "./Main/Main.jsx";
+import NavBar from "./NavBar/NavBar.jsx";
+import Footer from "./Footer/Footer.jsx";
 
-const Pokedex = () => {
-   return (
-      <>
-         <Header />
-         <PokemonList />;
-         <Footer />
-      </>
-   );
+const App = () => {
+    return (
+        <CssVarsProvider theme={theme}>
+            <PokedexProvider>
+                <NavBar />
+                <Main />
+                <Footer />
+            </PokedexProvider>
+        </CssVarsProvider>
+    );
 };
 
-export default Pokedex;
+export default App;
